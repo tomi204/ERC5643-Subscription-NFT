324b28dac28dbf9d59a19bec69a9aaee69c9d047
tomi204 <tomi204@outlook.com>
2022-12-10 20:49:31 -0300

    mint and renew function

--- contracts/ERC5643.sol
+++ contracts/ERC5643.sol
@@ -22,11 +22,34 @@ contract Subscription is IERC5643 {
     mapping(uint256 => bool) private _renewable;
     mapping (uint256 => uint256) _price;
 
+// @dev constructor
+     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+
+        
+    }
     //@dev function for support interface
     function supportsInterface(bytes4 interfaceId) public view virtual override  returns (bool) {
         returns super.supportsInterface(interfaceId) || interfaceId == type(IERC5643).interfaceId;
     }
-    constructor() {
-        
+
+    //@dev mint subscription function 
+    function mintSubscription(address owner, uint64 duration, uint256 price) public payable returns (uint256) {
+        require(msg.value == price, "Price is not correct");
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _mint(owner, newTokenId);
+        _expirations[newTokenId] = uint64(block.timestamp) + duration;
+        _renewable[newTokenId] = true;
+        _price[newTokenId] = price;
+        return newTokenId;
     }
+    //@dev renew subscription function
+    function renewSubscription(uint256 tokenId, uint64 duration) public payable override {
+        require(_isApprovedOrOwner(msg.sender, tokenId), "Caller is not owner nor approved");
+        require(_renewable[tokenId], "Subscription is not renewable");
+        require(msg.value == _price[tokenId], "Price is not correct");
+        _expirations[tokenId] = uint64(block.timestamp) + duration;
+        emit SubscriptionUpdate(tokenId, _expirations[tokenId]);
+    }
+   
 }
