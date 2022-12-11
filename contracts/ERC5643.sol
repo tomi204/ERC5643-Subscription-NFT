bf1ff54cbb1f078ef74faf6f326c056ec18df26e
tomi204 <tomi204@outlook.com>
2022-12-11 11:21:55 -0300

    cambios en la arquitectura por pedido del cliente

--- contracts/ERC5643.sol
+++ contracts/ERC5643.sol
@@ -15,7 +15,7 @@ interface IERC5643 {
     function expiresAt(uint256 tokenId) external view returns(uint64);
     function isRenewable(uint256 tokenId) external view returns(bool);
 }
-contract Subscription is IERC5643 {
+contract Subscription is IERC5643, ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, ERC721Burnable {
     //@dev state variables 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -32,39 +32,45 @@ contract Subscription is IERC5643 {
         
     }
     //@dev function for support interface
-    function supportsInterface(bytes4 interfaceId) public view virtual override  returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721ENUMERABLE)  returns (bool) {
         returns super.supportsInterface(interfaceId) || interfaceId == type(IERC5643).interfaceId;
+    }
+    //@dev function for look up expiration date
+    function expiresAt(uint256 tokenId) public view override returns(uint64) {
+        return expirations[tokenId];
     }
 
     //@dev mint subscription function 
-    function mintSubscription(address owner, uint64 duration, uint256 price) public payable returns (uint256) {
-        require(msg.value == price, "Price is not correct");
+    function mintSubscription(address to, uint64 duration) public onlyOwner returns(uint256) {
+        require(msg.value >= 0.5 ether);
         _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
-        _mint(owner, newTokenId);
-        _expirations[newTokenId] = uint64(block.timestamp) + duration;
-        _renewable[newTokenId] = true;
-        _price[newTokenId] = price;
+        _mint(to, newTokenId);
+      
+        expirations[newTokenId] = uint64(block.timestamp) + duration;
+        renewable[newTokenId] = true;
         return newTokenId;
     }
+    
     //@dev renew subscription function
     function renewSubscription(uint256 tokenId, uint64 duration) public payable override {
         require(_isApprovedOrOwner(msg.sender, tokenId), "Caller is not owner nor approved");
-        require(_renewable[tokenId], "Subscription is not renewable");
+        require(renewable[tokenId], "Subscription is not renewable");
         require(msg.value == _price[tokenId], "Price is not correct");
-        _expirations[tokenId] = uint64(block.timestamp) + duration;
-        emit SubscriptionUpdate(tokenId, _expirations[tokenId]);
+        expirations[tokenId] = uint64(block.timestamp) + duration;
+        renewable[tokenId] = true;
+        emit SubscriptionUpdate(tokenId, expirations[tokenId]);
     }
 
     //@dev cancel subscription function
     function cancelSubscription(uint256 tokenId) public payable override {
         require(_isApprovedOrOwner(msg.sender, tokenId), "Caller is not owner nor approved");
-        require(_renewable[tokenId], "Subscription is not renewable");
-        require(msg.value == _price[tokenId], "Price is not correct");
-        _renewable[tokenId] = false;
-        delete _expirations[tokenId];
-        delete _price[tokenId];
-        emit SubscriptionUpdate(tokenId, _expirations[tokenId]);
+        require(renewable[tokenId], "Subscription is not renewable");
+        renewable[tokenId] = false;
+        delete expirations[tokenId];
+        delete price[tokenId];
+        emit SubscriptionUpdate(tokenId, expirations[tokenId]);
     }
+    event SubscriptionUpdate(tokenId, expirations[tokenId]);
    
 }
